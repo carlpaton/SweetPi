199a072f919936b516cfa0a2d632bae542c7d5df
Carl <carl.paton@gmail.com>
2021-03-09 00:03:45 +1300

    ping datestamp, postman cleanup

--- src/SweetPi.Api/Controllers/PingController.cs
+++ src/SweetPi.Api/Controllers/PingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace SweetPi.Api.Controllers
 {
@@ -7,7 +8,7 @@ namespace SweetPi.Api.Controllers
         [HttpGet("/ping")]
         public IActionResult Get()
         {
-            return Ok("SweetPi.Api is running...");
+            return Ok($"SweetPi.Api is running... {DateTime.Now}");
         }
     }
 }
